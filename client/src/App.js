be50d31dbe55b99a047851a94c251c4ab9099416
Matt Haddon <matthaddon5@gmail.com>
2020-12-28 12:30:57 +0000

    [ADD] initial components

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h3>Just kidding</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Portfolio</a>
+        </li>
+        <li>
+          <a>Technologies</a>
+        </li>
+        <li>
+          <a>Contact Me</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
